1bec5c92c69a22135ceac335b241c06c9167571e
OutrageousPotato <jonathan@gibsonnotes.com>
2017-08-14 14:39:28 -0700

    Item 46 uploaded

---
+++ item46.py
@@ -0,0 +1,15 @@
+print("\nDrill #1: \n")
+
+for i in range(4):
+    print (i)
+
+print("\nDrill #2: \n")
+
+for i in range(3,-1,-1):
+    print(i)
+
+
+print("\nDrill #3: \n")
+
+for i in range(8,1,-2):
+    print(i)
